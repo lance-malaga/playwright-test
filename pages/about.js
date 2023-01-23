2bae7217a56cc94c33462844547ddf947237db94
lance-malaga <lmalaga@my.bcit.ca>
2023-01-23 11:43:51 -0800

    inital commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function AboutPage () {
+    return (
+        <>
+            About Page
+            <Link href='/'>Go Back</Link>
+        </>
+
+    )
+}
